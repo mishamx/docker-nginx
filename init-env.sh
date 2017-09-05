094763040fe0804dd723843538ad25b0f3556ebf
Mikhail Mangushev <mishamx@gmail.com>
2017-09-05 17:16:14 +0300

    Added sites-enabled to envplate

--- init-env.sh
+++ init-env.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
 cp -R /etc/nginx-source/ /etc/nginx/
+/usr/local/bin/ep -v /etc/nginx/sites-enabled/*.conf
 /usr/local/bin/ep -v /etc/nginx/*.conf -- /usr/sbin/nginx -g "daemon off;"
